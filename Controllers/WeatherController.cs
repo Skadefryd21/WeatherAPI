ef537f82d86f7a2064bffcb8b83a12b9d878b9d3
Niko <Niko@DESKTOP-2R0EG79>
2023-09-18 13:15:02 +0200

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAPI.Models;
+using Newtonsoft.Json;
+using System;
+using Microsoft.AspNetCore.Mvc.Diagnostics;
+
+namespace WeatherAPI.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+
+        private readonly HttpClient _httpClient;
+
+        public WeatherController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetWeather(WeatherModel input)
+        {
+
+            HttpResponseMessage response = _httpClient.GetAsync(@"https://api.openweathermap.org/data/2.5/weather?lat=" + input.Lat + "&lon=" + input.Lon + "&appid=cde7e8e92f266f62433a2bf35ee88590").Result;
+
+            WeatherModel temp = new WeatherModel();
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            json.Split(",");//Where to split?
+
+            //TODO: After split, figure out how to assign values to correct properties in "temp"
+
+            return View(temp);
+        }
+            public IActionResult Index()
+        {
+            return View("./Views/Home/Index.cshtml");
+        }
+    }
+}
---
+++ Models/WeatherModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace WeatherAPI.Models
+{
+    public class WeatherModel
+    {
+        public string? Lon { get; set; }
+        public string? Lat { get; set; }
+
+        public string? Lonlat
+        {
+            get
+            {
+                string lonlat = @"Longitude: " + Lon + " Latitude: " + Lat;
+                return lonlat;
+            }
+        }
+        [DisplayName("Current weather")]
+        public string? Weather { get; set; }
+        public bool? Rain { get; set; }
+        public string? Country { get; set; }
+    }
+}
